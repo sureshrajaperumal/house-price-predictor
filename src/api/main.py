13765954a788e303a23225730c59382df76adb54
Sureshraja Perumal <psureshraja+git@gmail.com>
2025-09-30 18:51:26 +0530

    Changed

--- src/api/main.py
+++ src/api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from inference import predict_price, batch_predict
 from schemas import HousePredictionRequest, PredictionResponse
+from prometheus_fastapi_instrumentator 
 
 # Initialize FastAPI app with metadata
 app = FastAPI(
@@ -32,6 +33,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+Instrumentator().instrument(app).expose(app)
 # Health check endpoint
 @app.get("/health", response_model=dict)
 async def health_check():
