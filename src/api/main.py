b34792432f6327d47744b1c144410a34cc8d494d
Sureshraja Perumal <psureshraja+git@gmail.com>
2025-09-30 19:18:15 +0530

    added the prometheous_fastapi_instrumentator

--- src/api/main.py
+++ src/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from inference import predict_price, batch_predict
 from schemas import HousePredictionRequest, PredictionResponse
-from prometheus_fastapi_instrumentator 
+from prometheus_fastapi_instrumentator import Instrumentator
 
 # Initialize FastAPI app with metadata
 app = FastAPI(
